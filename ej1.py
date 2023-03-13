ccd963bb1a73989de825213ee6dba74d07126137
Agirre8 <inagi30@gmail.com>
2023-03-13 16:27:59 +0100

    ej1 acabado

--- ej1.py
+++ ej1.py
@@ -7,3 +7,22 @@ for i in range(1,50):
 
 tabla.sort()
 print (tabla)
+
+def busquedaDicotómica(tabla, x, low, high):
+
+    while low <= high:
+        mid = low + (high - low)//2
+        if tabla[mid] == x:
+            return mid
+        elif tabla[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+result = busquedaDicotómica(tabla, x, 0, len(tabla)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
