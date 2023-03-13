f92d60e0effbc7cb8faa514b62ae01983d7f852b
Agirre8 <inagi30@gmail.com>
2023-03-13 16:55:01 +0100

    ej 2

--- ej2.py
+++ ej2.py
@@ -1,11 +1,19 @@
 
-def esPalindromo(word):
-    if len(word) < 2: 
+
+palabra = input("Introduzca una palabra: ")
+
+def esPalindromo(palabra):
+    palabra = palabra.lower()
+    a,b = 'áéíóúü','aeiouu'
+    trans = str.maketrans(a,b)
+    print(palabra.translate(trans))
+    palabra = palabra.translate(trans)
+
+    if len(palabra) < 2: 
         print("La palabra es un palíndromo")
-    if word[0] != word[-1]: 
+    if palabra[0] != palabra[-1]: 
         print("La palabra NO es un palíndromo")
     else:
         print("La palabra es un palíndromo")
-
-
+        
 esPalindromo(palabra)
